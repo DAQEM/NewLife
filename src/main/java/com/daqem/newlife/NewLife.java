cc518ed37a6581f34f5324e8638431f3f1753f50
DAQEM <daqemyt@gmail.com>
2023-05-31 15:58:07 +0200

    Initial commit

---
+++ src/main/java/com/daqem/newlife/NewLife.java
@@ -0,0 +1,15 @@
+package com.daqem.newlife;
+
+import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class NewLife implements ModInitializer {
+
+    public static final Logger LOGGER = LogManager.getLogger();
+
+    @Override
+    public void onInitialize() {
+        LOGGER.info("NewLife Initialized!");
+    }
+}
